3288fec68d500d6fd1a32a417a5d78557b3e4303
MohammadMahdi Afrouzi <irimmamad@gmail.com>
2026-05-18 19:42:13 +0200

    Final completed project version

---
+++ result.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "result.h"
+#include "log.h"
+
+void saveResult(struct Student student, int score, int total) {
+
+    FILE *file = fopen("results.txt", "a");
+
+    if (file == NULL) {
+        printf("Error opening results file.\n");
+        return;
+    }
+
+    fprintf(file, "%d,%s,%s,%d/%d\n",
+            student.id,
+            student.name,
+            student.surname,
+            score,
+            total);
+
+    fclose(file);
+
+    printf("Result saved successfully.\n");
+    writeLog("Result saved");
+}
